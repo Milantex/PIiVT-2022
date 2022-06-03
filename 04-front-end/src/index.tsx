2be72749503fe4256e990ec775d898e669e4e28a
Milan Tair <milan.tair@gmail.com>
2022-06-03 14:46:43 +0200

    Init the front end application

---
+++ 04-front-end/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './common/reportWebVitals';
+import Application from './components/Application/Application';
+import './index.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <Application />
+  </React.StrictMode>
+);
+
+reportWebVitals(console.log);
