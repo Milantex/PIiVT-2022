6da3150696011e3b9dab8f3085d7039d9d3a5579
Milan Tair <milan.tair@gmail.com>
2022-06-06 15:11:10 +0200

    Implement a method to add a new category inline

--- 04-front-end/src/components/Administrator/Dashboard/AdminCategoryList.tsx
+++ 04-front-end/src/components/Administrator/Dashboard/AdminCategoryList.tsx
@@ -11,6 +11,7 @@ interface IAdminCategoryListRowProperties {
 export default function AdminCategoryList() {
     const [ categories, setCategories ] = useState<ICategory[]>([]);
     const [ errorMessage, setErrorMessage ] = useState<string>("");
+    const [ showAddNewCategory, setShowAddNewCategory ] = useState<boolean>(false);
 
     function AdminCategoryListRow(props: IAdminCategoryListRowProperties) {
         const [ name, setName ] = useState<string>(props.category.name);
@@ -56,6 +57,56 @@ export default function AdminCategoryList() {
         );
     }
 
+    function AdminCategoryAddRow() {
+        const [ name, setName ] = useState<string>("");
+
+        const nameChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+            setName( e.target.value );
+        }
+
+        const doAddCategory = (e: any) => {
+            api("post", "/api/category/", "administrator", { name })
+            .then(res => {
+                if (res.status === 'error') {
+                    return setErrorMessage("Could not add this category!");
+                }
+
+                loadCategories();
+
+                setName("");
+                setShowAddNewCategory(false);
+            });
+        }
+
+        return (
+            <tr>
+                <td> </td>
+                <td>
+                    <div className="input-group">
+                        <input className="form-control form-control-sm"
+                               type="text"
+                               onChange={ e => nameChanged(e) }
+                               value={ name } />
+                        { name.trim().length >= 4 && name.trim().length <= 32
+                            ? <button className="btn btn-primary btn-sm" onClick={ e => doAddCategory(e) }>
+                                  Save
+                              </button>
+                            : ''
+                        }
+                    </div>
+                </td>
+                <td>
+                    <button className="btn btn-danger btn-sm" onClick={ () => {
+                        setShowAddNewCategory(false);
+                        setName("");
+                    } }>
+                        Cancel
+                    </button>
+                </td>
+            </tr>
+        );
+    }
+
     const loadCategories = () => {
         api("get", "/api/category", "administrator")
         .then(apiResponse => {
@@ -78,18 +129,23 @@ export default function AdminCategoryList() {
         <div>
             { errorMessage && <p>Error: { errorMessage }</p> }
             { !errorMessage &&
-                <table className="table table-bordered table-striped table-hover table-sm">
-                    <thead>
-                        <tr>
-                            <th className="category-row-id">ID</th>
-                            <th>Name</th>
-                            <th className="category-row-options">Options</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { categories.map(category => <AdminCategoryListRow key={ "category-row-" + category.categoryId } category={ category } />) }
-                    </tbody>
-                </table>
+                <div>
+                    <button className="btn btn-primary btn-sm" onClick={() => setShowAddNewCategory(true)}>Add new category</button>
+
+                    <table className="table table-bordered table-striped table-hover table-sm mt-3">
+                        <thead>
+                            <tr>
+                                <th className="category-row-id">ID</th>
+                                <th>Name</th>
+                                <th className="category-row-options">Options</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { showAddNewCategory && <AdminCategoryAddRow /> }
+                            { categories.map(category => <AdminCategoryListRow key={ "category-row-" + category.categoryId } category={ category } />) }
+                        </tbody>
+                    </table>
+                </div>
             }
         </div>
     );
